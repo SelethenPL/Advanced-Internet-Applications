a6b7515f742871bfea26e0785dd6d86580710098
Łukasz D <lukisd@op.pl>
2020-04-12 22:03:25 +0200

    First contact with jQuery
    Simple jQuerry sites.
    First contact with AJAX.
    Simple getter for Json file.
    Simple app using Promise to get http request.

---
+++ 3.jQuery/Promise/get.js
@@ -0,0 +1,21 @@
+function get(url) {
+  return new Promise(function(resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+
+    req.onload = function() {
+      if (req.status == 200) {
+        resolve(req.response);
+      }
+      else {
+        reject(Error(req.statusText));
+      }
+    };
+
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    req.send();
+  });
+}
